a1e7eca138ff94ee6a00b7faa26e416de63245b7
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-08-13 17:20:30 +0300

    dynamodb data retrieval

--- data_retrieval/data_retrievers/dynamodb_retriever.py
+++ data_retrieval/data_retrievers/dynamodb_retriever.py
@@ -1,5 +1,22 @@
 from .abstract_retriever import AbstractRetriever
 import boto3
 class DynamoDBRetriever(AbstractRetriever):
-    def retrieve(self, device: str, data_from: str, data_to: str) -> dict:
-        pass
+    def __init__(self):
+        # TODO: use session?
+        self.client = boto3.client('dynamodb')
+
+    def retrieve(self, device: str, data_from: str, data_to: str, attributes: tuple | None = None, raw: bool = True) -> dict:
+        response = self.client.query(
+            TableName="aq_measurements",
+            Select="ALL_ATTRIBUTES",
+            KeyConditionExpression='device_id = :device_id AND #time BETWEEN :data_from AND :data_to',
+            ExpressionAttributeNames={
+                '#time': 'time'
+            },
+            ExpressionAttributeValues={
+                ':device_id': {'S': device},
+                ':data_from': {'S': data_from},
+                ':data_to': {'S': data_to}
+            }
+        )
+        print(response)
--- data_retrieval/main.py
+++ data_retrieval/main.py
@@ -1,12 +1,15 @@
-from data_retrievers import TimestreamDBRetriever
+from data_retrievers import TimestreamDBRetriever, DynamoDBRetriever
 def main():
     device = "test_1"
     data_from = "2024-08-13 11:12:04"
     data_to = "2024-08-13 11:12:06"
     attributes = ("co", "temperature", "humidity")
 
-    retriever = TimestreamDBRetriever()
-    retriever.retrieve(device, data_from, data_to, attributes, False) # TODO: raw => True!!!
+    # retriever_timestream = TimestreamDBRetriever()
+    # retriever_timestream.retrieve(device, data_from, data_to, attributes, True)
+
+    retriever_dynamodb = DynamoDBRetriever()
+    retriever_dynamodb.retrieve(device, data_from, data_to, attributes, True)
 
 
 if __name__ == '__main__':
