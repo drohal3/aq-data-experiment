c3c00a7805ae08a3b3486c092a27886926527a93
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-09-07 15:32:45 +0300

    renamed dynamodb table

--- data_retrieval/data_retrievers/dynamodb_retriever.py
+++ data_retrieval/data_retrievers/dynamodb_retriever.py
@@ -14,9 +14,11 @@ class DynamoDBRetriever(AbstractRetriever):
         # TODO: use session?
         self.client = boto3.client('dynamodb')
 
-    def retrieve(self, device: str, data_from: str, data_to: str, attributes: tuple | None = None, raw: bool = True) -> dict:
+    def retrieve(
+            self, device: str, data_from: str, data_to: str, attributes: tuple | None = None, raw: bool = True
+    ) -> dict:
         response = self.client.query(
-            TableName="aq_measurements",
+            TableName="aq_experiment",
             Select="ALL_ATTRIBUTES",
             KeyConditionExpression='device_id = :device_id AND #time BETWEEN :data_from AND :data_to',
             ExpressionAttributeNames={
@@ -29,3 +31,4 @@ class DynamoDBRetriever(AbstractRetriever):
             }
         )
         print(response)
+        return {}
