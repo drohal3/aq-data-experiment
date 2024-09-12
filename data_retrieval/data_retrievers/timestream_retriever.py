45ada089ec5ccfbd6dcf39536608dff18d2e7a9a
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-09-12 16:25:19 +0300

    timestream fixed retriever

--- data_retrieval/data_retrievers/timestream_retriever.py
+++ data_retrieval/data_retrievers/timestream_retriever.py
@@ -5,23 +5,21 @@ class TimestreamDBRetriever(AbstractRetriever):
     def __init__(self):
         self.client = boto3.client('timestream-query')
 
-    def _retrieve_raw(self, device: str, data_from: str, data_to: str, attributes: tuple | None = None) -> dict:
+    def _retrieve_raw(self, device: str, data_from: str, data_to: str, attributes: list | None = None) -> dict:
         value_string = ""
         if attributes is not None:
             value_string = ', '.join(f"'{value}'" for value in attributes)
-        measurement_condition = "" if attributes is None else f"WHERE measure_name in ({value_string})"
+        measurement_condition = "" if attributes is None else f"measure_name in ({value_string}) AND"
 
         query_string = f"""
             SELECT measure_name, time, measure_value::double
             FROM "aq-time-stream"."aq_data"
-            {measurement_condition}
-            AND device_id = '{device}'
+            WHERE {measurement_condition}
+            device_id = '{device}'
             AND time >= '{data_from}.000000000' 
             AND time <= '{data_to}.000000000'
             ORDER BY time, measure_name
         """
-
-        print(query_string)
 
         response = self.client.query(QueryString=query_string)
 
@@ -36,6 +34,7 @@ class TimestreamDBRetriever(AbstractRetriever):
 
         # WARNING: the code below assumes the rows are sorted by time
         # WARNING: the code below assumes that only one device is queried
+        # WARNING: the code below assumes that all values are floats/doubles
         for row in rows:
             data = row["Data"]
             time = data[1]["ScalarValue"][:19]
@@ -45,6 +44,9 @@ class TimestreamDBRetriever(AbstractRetriever):
                     processed.append(last_row)
                 last_row = {"time": time}
             measure_name = data[0]["ScalarValue"]
+            if measure_name == "device_id" or measure_name == "time":
+                continue
+
             value = data[2]["ScalarValue"]
             last_row[measure_name] = value
 
