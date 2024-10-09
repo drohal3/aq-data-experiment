15592da6da288549404e17df6de6763ff39e2c09
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-10-09 17:13:47 +0300

    dynamodb retriever init

--- data_retrieval/data_retrievers/__init__.py
+++ data_retrieval/data_retrievers/__init__.py
@@ -3,13 +3,11 @@ from .timestream_retriever import TimestreamDBRetriever
 from .dynamodb_retriever import DynamoDBRetriever
 from .kinesis_retriever import KinesisRetriever
 from .s3_retriever import S3Retriever
-from .dynamodb_retriever_v2 import DynamoDBRetrieverV2
 
 __all__ = [
     "AbstractRetriever",
     "TimestreamDBRetriever",
     "DynamoDBRetriever",
-    "DynamoDBRetrieverV2",
     "KinesisRetriever",
     "S3Retriever"
 ]
